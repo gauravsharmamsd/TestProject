ce2601e7c91b2142fdd47dfea6480b5ca3ae2582
Gaurav Kaushik <gauravsharmamsd@gmail.com>
2022-03-21 22:50:45 -0700

    First commit

---
+++ SpringMvcBoot/src/test/java/com/worldline/SpringMvcBoot/SpringMvcBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldline.SpringMvcBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
